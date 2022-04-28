bae1491155d7c0fb59a4ab5598f5935d7f44d1e1
Frank van den Broek <s1130543@student.hsleiden.nl>
2022-04-28 16:02:29 +0200

    add daisyui

--- apps/security-check/tailwind.config.js
+++ apps/security-check/tailwind.config.js
@@ -14,5 +14,19 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        getBigMarketingThemeLight: {
+          // TODO: create theme, see https://daisyui.com/theme-generator/
+          primary: '#a991f7',
+          secondary: '#f6d860',
+          accent: '#37cdbe',
+          neutral: '#3d4451',
+          'base-100': '#ffffff',
+        },
+      },
+    ],
+  },
 };
