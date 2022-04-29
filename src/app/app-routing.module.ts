1d6fff8ec5c5441160d493aa27c4d509498df319
Hayyan55 <hayan_mez@hotmail.co.uk>
2022-04-29 18:34:06 +0200

    updated read me

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import {MainScanComponent} from "./main-scan/main-scan.component";
 import {LoadScanComponent} from "./load-scan/load-scan.component";
 import {ScanResultComponent} from "./scan-result/scan-result.component";
-import {FaqPageComponent} from "./faq-page/faq-page.component";
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: 'home', component: MainScanComponent},
   {path:'loading...', component: LoadScanComponent},
   {path: 'scan-results', component: ScanResultComponent},
-  {path: 'faq', component: FaqPageComponent},
   {path: '**', redirectTo:'home', pathMatch: 'full'}
 ];
 
